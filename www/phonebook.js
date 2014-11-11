79593c85f41d0cfe2b5bc2e02cba4d1128689f05
dinasalah <dina.salah@anzmasoft.com>
2014-11-11 14:38:51 +0200

    Update and rename phonebook_emails.js to phonebook.js

--- www/phonebook.js
+++ www/phonebook.js
@@ -1,9 +1,9 @@
-var phonebook_emils= {
+var phonebook= {
     listEmails: function(title, successCallback, errorCallback) {
  	cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
-            'phonebookEmails', // mapped to our native Java class called "Calendar"
+            'Phonebook', // mapped to our native Java class called "Calendar"
             'ListEmails', // with this action name
             [{                  // and this array of custom arguments to create our entry
                 "title": title
@@ -12,4 +12,4 @@ var phonebook_emils= {
      }
 }
 
-module.exports = phonebook_emils;
+module.exports = phonebook;
